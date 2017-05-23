d1c346ca31c8fc4ed4994ba6c500179f6efbf520
yp <1137462065@qq.com>
2017-05-24 02:39:54 +0800

    location

---
+++ app/src/main/java/com/example/yp/noisemap/LocationActivity.java
@@ -0,0 +1,241 @@
+package com.example.yp.noisemap;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Application;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.location.Poi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LocationActivity extends AppCompatActivity {
+    private final int SDK_PERMISSION_REQUEST = 127;
+    private String permissionInfo;
+    private LocationService locationService;
+    private TextView LocationResult;
+    private Button startLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.location);
+        LocationResult = (TextView) findViewById(R.id.textView1);
+        LocationResult.setMovementMethod(ScrollingMovementMethod.getInstance());
+        startLocation = (Button) findViewById(R.id.addfence);
+        getPersimmions();
+    }
+
+    @TargetApi(25)
+    private void getPersimmions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ArrayList<String> permissions = new ArrayList<String>();
+            /***
+             * 定位权限为必须权限，用户如果禁止，则每次进入都会申请
+             */
+            // 定位精确位置
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+            }
+            /*
+			 * 读写权限和电话状态权限非必要权限(建议授予)只会申请一次，用户同意或者禁止，只会弹一次
+			 */
+            // 读写权限
+            if (addPermission(permissions, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                permissionInfo += "Manifest.permission.WRITE_EXTERNAL_STORAGE Deny \n";
+            }
+            // 读取电话状态权限
+            if (addPermission(permissions, Manifest.permission.READ_PHONE_STATE)) {
+                permissionInfo += "Manifest.permission.READ_PHONE_STATE Deny \n";
+            }
+
+            if (permissions.size() > 0) {
+                requestPermissions(permissions.toArray(new String[permissions.size()]), SDK_PERMISSION_REQUEST);
+            }
+        }
+    }
+
+    @TargetApi(25)
+    private boolean addPermission(ArrayList<String> permissionsList, String permission) {
+        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) { // 如果应用没有获得对应权限,则添加到列表中,准备批量申请
+            if (shouldShowRequestPermissionRationale(permission)) {
+                return true;
+            } else {
+                permissionsList.add(permission);
+                return false;
+            }
+
+        } else {
+            return true;
+        }
+    }
+
+    @TargetApi(25)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // TODO Auto-generated method stub
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        locationService = ((LocationApplication) getApplication()).locationService;
+        startLocation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (startLocation.getText().toString().equals(getString(R.string.startlocation))) {
+                    locationService.start();// 定位SDK
+                    // start之后会默认发起一次定位请求，开发者无须判断isstart并主动调用request
+                    startLocation.setText(getString(R.string.stoplocation));
+                } else {
+                    locationService.stop();
+                    startLocation.setText(getString(R.string.startlocation));
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        locationService.unregisterListener(mListener); //注销掉监听
+        locationService.stop(); //停止定位服务
+        super.onStop();
+    }
+
+    public void logMsg(String str) {
+        final String s = str;
+        try {
+            if (LocationResult != null) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        LocationResult.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                LocationResult.setText(s);
+                            }
+                        });
+
+                    }
+                }).start();
+            }
+            //LocationResult.setText(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private BDLocationListener mListener = new BDLocationListener() {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            // TODO Auto-generated method stub
+            if (null != location && location.getLocType() != BDLocation.TypeServerError) {
+                StringBuffer sb = new StringBuffer(256);
+                sb.append("time : ");
+                /**
+                 * 时间也可以使用systemClock.elapsedRealtime()方法 获取的是自从开机以来，每次回调的时间；
+                 * location.getTime() 是指服务端出本次结果的时间，如果位置不发生变化，则时间不变
+                 */
+                sb.append(location.getTime());
+                sb.append("\nlocType : ");// 定位类型
+                sb.append(location.getLocType());
+                sb.append("\nlocType description : ");// *****对应的定位类型说明*****
+                sb.append(location.getLocTypeDescription());
+                sb.append("\nlatitude : ");// 纬度
+                sb.append(location.getLatitude());
+                sb.append("\nlontitude : ");// 经度
+                sb.append(location.getLongitude());
+                sb.append("\nradius : ");// 半径
+                sb.append(location.getRadius());
+                sb.append("\nCountryCode : ");// 国家码
+                sb.append(location.getCountryCode());
+                sb.append("\nCountry : ");// 国家名称
+                sb.append(location.getCountry());
+                sb.append("\ncitycode : ");// 城市编码
+                sb.append(location.getCityCode());
+                sb.append("\ncity : ");// 城市
+                sb.append(location.getCity());
+                sb.append("\nDistrict : ");// 区
+                sb.append(location.getDistrict());
+                sb.append("\nStreet : ");// 街道
+                sb.append(location.getStreet());
+                sb.append("\naddr : ");// 地址信息
+                sb.append(location.getAddrStr());
+                sb.append("\nUserIndoorState: ");// *****返回用户室内外判断结果*****
+                sb.append(location.getUserIndoorState());
+                sb.append("\nDirection(not all devices have value): ");
+                sb.append(location.getDirection());// 方向
+                sb.append("\nlocationdescribe: ");
+                sb.append(location.getLocationDescribe());// 位置语义化信息
+                sb.append("\nPoi: ");// POI信息
+                if (location.getPoiList() != null && !location.getPoiList().isEmpty()) {
+                    for (int i = 0; i < location.getPoiList().size(); i++) {
+                        Poi poi = (Poi) location.getPoiList().get(i);
+                        sb.append(poi.getName() + ";");
+                    }
+                }
+                if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+                    sb.append("\nspeed : ");
+                    sb.append(location.getSpeed());// 速度 单位：km/h
+                    sb.append("\nsatellite : ");
+                    sb.append(location.getSatelliteNumber());// 卫星数目
+                    sb.append("\nheight : ");
+                    sb.append(location.getAltitude());// 海拔高度 单位：米
+                    sb.append("\ngps status : ");
+                    sb.append(location.getGpsAccuracyStatus());// *****gps质量判断*****
+                    sb.append("\ndescribe : ");
+                    sb.append("gps定位成功");
+                } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+                    // 运营商信息
+                    if (location.hasAltitude()) {// *****如果有海拔高度*****
+                        sb.append("\nheight : ");
+                        sb.append(location.getAltitude());// 单位：米
+                    }
+                    sb.append("\noperationers : ");// 运营商信息
+                    sb.append(location.getOperators());
+                    sb.append("\ndescribe : ");
+                    sb.append("网络定位成功");
+                } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                    sb.append("\ndescribe : ");
+                    sb.append("离线定位成功，离线定位结果也是有效的");
+                } else if (location.getLocType() == BDLocation.TypeServerError) {
+                    sb.append("\ndescribe : ");
+                    sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+                } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                    sb.append("\ndescribe : ");
+                    sb.append("网络不同导致定位失败，请检查网络是否通畅");
+                } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                    sb.append("\ndescribe : ");
+                    sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+                }
+                logMsg(sb.toString());
+            }
+        }
+
+        public void onConnectHotSpotMessage(String s, int i) {
+        }
+    };
+
+}
+
+
